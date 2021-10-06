e5c75eaff443175dea6c8d020e217c42671a0cf6
meenathiruppathi <91307967+meenathiruppathi@users.noreply.github.com>
2021-10-05 23:56:03 -0400

    Add files via upload

---
+++ moviecontroller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcapplication.Controllers
+{
+    public class moviecontroller : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
